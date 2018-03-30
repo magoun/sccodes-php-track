0a81e03c5bfbabf7679abc4033c063b3bfae2175
Creighton Magoun <magoun@gmail.com>
2018-03-30 03:20:18 +0000

    setup basic routing to controller

--- track-project/routes/web.php
+++ track-project/routes/web.php
@@ -11,6 +11,10 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', 'apiController@debugOrgs');
+Route::get('/orgs', 'apiController@showOrgs');
+Route::get('/events', 'apiController@showEvents');
