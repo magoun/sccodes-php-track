78c93d7bb25ee3aed76874df55f25bba4b083381
Creighton Magoun <magoun@gmail.com>
2018-04-06 23:30:51 +0000

    update tab stop to two spaces

--- track-project/resources/views/events.blade.php
+++ track-project/resources/views/events.blade.php
@@ -3,14 +3,14 @@
 @section('title', 'Events')
 
 @section('content')
-    <ul>
-        @foreach ($events as $event)
-            <li>{{ $event->event_name }} hosted by {{ $event->group_name }}
-                <ul>
-                    <li>Time: <?= DateTime::createFromFormat('Y-m-d\TH:i:s\Z', $event->time)
-                                    ->format('D j M y') ?></li>
-                </ul>
-            </li>
-        @endforeach
-    </ul>
+	<ul>
+		@foreach ($events as $event)
+			<li>{{ $event->event_name }} hosted by {{ $event->group_name }}
+				<ul>
+					<li>Time: <?= DateTime::createFromFormat('Y-m-d\TH:i:s\Z', 
+								$event->time)->format('D j M y') ?></li>
+				</ul>
+			</li>
+		@endforeach
+	</ul>
 @endsection
