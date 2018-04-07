7f7cba83296ea23df75f77a7ca3308ce85c0c33b
Creighton Magoun <magoun@gmail.com>
2018-04-07 06:18:03 +0000

    Add google calendar integration framework

--- track-project/resources/views/events.blade.php
+++ track-project/resources/views/events.blade.php
@@ -24,11 +24,30 @@
 			  <strong>
 			    {{ $event->event_name }} hosted by {{ $event->group_name }}
 		    </strong>
-				<ul>
-					<li><strong>Time:</strong> <?= DateTime::
-					  createFromFormat('Y-m-d\TH:i:s\Z', $event->time)
-					  ->format('g:i A, D j M y') ?></li>
-				</ul>
+		    
+		    <?php
+		      $event_time = DateTime::createFromFormat('Y-m-d\TH:i:s\Z', 
+		        $event->time);
+		      $start_time = $event_time->format('Ymd\THis\Z');
+		      echo $start_time;
+		      $event_time->add(new DateInterval('PT3H'));
+		      $end_time = $event_time->format('Ymd\THis\Z');
+		      echo $end_time;
+		    
+		      $calendar_url = "http://www.google.com/calendar/event?action=TEMPLATE&";
+		      $calendar_url .= 'text=' . urlencode($event->event_name) . '&';
+		      $calendar_url .= "dates=$start_time/$end_time&";
+		      $calendar_url .= 'details=' . urlencode( strip_tags( $event->description )) . '&';
+		      $calendar_url .= "location=&";
+		      $calendar_url .= "trp=false&";
+		    ?>
+		    <a href=<?= $calendar_url; ?> target="_blank">
+  				<ul>
+  					<li><strong>Time:</strong> <?= DateTime::
+  					  createFromFormat('Y-m-d\TH:i:s\Z', $event->time)
+  					  ->format('g:i A, D j M y') ?></li>
+  				</ul>
+				</a>
 			</li>
 		@endforeach
 	</ul>
