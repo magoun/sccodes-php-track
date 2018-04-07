0208b8dcf72982222d052c2ef0fdbaff277da797
Creighton Magoun <magoun@gmail.com>
2018-04-07 04:45:11 +0000

    Add links for filtering by month

--- track-project/resources/views/events.blade.php
+++ track-project/resources/views/events.blade.php
@@ -3,12 +3,31 @@
 @section('title', 'Events')
 
 @section('content')
+	<h1>Events</h1>
+	<h3>Choose a month to filter</h3>
+	
+	<a href="<?= explode("?", $_SERVER['REQUEST_URI'])[0] ?>">
+	  Clear filter
+	</a>
+	<br />
+	
+	@foreach( $months as $month )
+	  <a href=<?= '?month=' . rawurlencode($month) ?>>
+	    <?= $month ?>
+    </a>
+    <br />
+	@endforeach
+	
 	<ul>
-		@foreach ($events as $event)
-			<li>{{ $event->event_name }} hosted by {{ $event->group_name }}
+		@foreach( $events as $event )
+			<li>
+			  <strong>
+			    {{ $event->event_name }} hosted by {{ $event->group_name }}
+		    </strong>
 				<ul>
-					<li>Time: <?= DateTime::createFromFormat('Y-m-d\TH:i:s\Z', 
-								$event->time)->format('D j M y') ?></li>
+					<li><strong>Time:</strong> <?= DateTime::
+					  createFromFormat('Y-m-d\TH:i:s\Z', $event->time)
+					  ->format('g:i A, D j M y') ?></li>
 				</ul>
 			</li>
 		@endforeach
