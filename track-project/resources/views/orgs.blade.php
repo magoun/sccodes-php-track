ffb37431091a671abab7f5cae85502f68967373e
Creighton Magoun <magoun@gmail.com>
2018-04-15 10:25:30 -0500

    Add more detail and links to organizations

--- track-project/resources/views/orgs.blade.php
+++ track-project/resources/views/orgs.blade.php
@@ -4,6 +4,13 @@
 
 @section('content')
     @foreach ($orgs as $org)
-        <li>{{ $org->title }}</li>
+        <li>
+            <a href="{{ getOrgWebsite( $org ) }}">{{ $org->title }}</a>
+            @if ($org->field_org_status == "Active")
+                - {{ $org->field_city }}
+            @else
+                - {{ $org->field_org_status }}
+            @endif
+        </li>
     @endforeach
 @endsection
